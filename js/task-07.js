5a5d9e4e84a5ac4bad9e65615504e30a925e1600
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-05-25 21:56:04 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,6 @@
 const inputValue = document.querySelector('#font-size-control')
 const textSize = document.querySelector('#text')
-
-setOutput()
-
 inputValue.addEventListener("input", setOutput)
-
 function setOutput () {
    textSize.style.fontSize = inputValue.value + `px`
 }
